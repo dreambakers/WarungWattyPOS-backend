ff572e19f18b71c8427b551b95cf72e5f2e321a7
Mahad Amir <mamir3012@gmail.com>
2020-03-18 20:12:43 +0500

    Setup for remote databsase

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
-dbUrl = 'mongodb://localhost:27017/WarungWatty';
+dbUrl = process.env.MONGODB_URI || 'mongodb://localhost:27017/WarungWatty';
 
 module.exports = {
-    dbConnectionUrl: process.env.NODE_ENV === 'test' ? dbUrl + 'Test': process.env.MONGODB_URI || dbUrl
+    dbConnectionUrl: process.env.NODE_ENV === 'test' ? dbUrl + 'Test' : dbUrl
 }
