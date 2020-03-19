48ac5a738f99b15279e141ec9365c4edf3fa1f5c
Mahad Amir <mamir3012@gmail.com>
2020-03-19 10:10:32 +0500

    More cors testing

--- app.js
+++ app.js
@@ -7,6 +7,15 @@ app.use(cors());
 app.options('*', cors());
 app.use(bodyParser.json());
 
+app.use((req, res, next) => { // this runs before each route
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'x-auth,x-filesize,Content-Type');
+    res.setHeader('Access-Control-Expose-Headers', 'x-auth,Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => { res.send('Hello world!') });
